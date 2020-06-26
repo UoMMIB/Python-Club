dba6623ec6a828912537e4416d6cb8759ddaaa5f
James Engleback <jamesengleback@hotmail.co.uk>
2020-06-26 15:05:33 +0100

    not sure what I did

--- Tutorials/quick_plot.py
+++ Tutorials/quick_plot.py
@@ -1,12 +1,16 @@
 import matplotlib.pyplot as plt
 import math
 
+'''
+Plot sin(x)
+'''
 def f(x):
+    # f(x) = sin(x)
     return math.sin(x)
 
 
-x = [i/100 for i in range(0,1000)]
-y = list(map(f,x))
+x = [i/100 for i in range(0,1000)] # 0:10, step size:0.1
+y = list(map(f,x)) # f(x)
 
-plt.plot(x,y, color='k', linestyle='--')
+plt.plot(x,y)
 plt.show()
